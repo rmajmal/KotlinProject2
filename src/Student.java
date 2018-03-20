6f7e6905686c182c21d812de228d256c01a6a296
rmajmal <rmajmal@hotmail.com>
2018-03-21 01:00:43 +0500

    Trying to Push Commit after Git Config

---
+++ src/Student.java
@@ -0,0 +1,41 @@
+import java.util.Objects;
+
+public class Student {
+    int id;
+    String rollNumber;
+
+    public Student(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id &&
+                Objects.equals(rollNumber, student.rollNumber);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, rollNumber);
+    }
+}
